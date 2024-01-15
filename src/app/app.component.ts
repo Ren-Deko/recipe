50b50a149de60956ff7b5f9a4b95f897934c1a82
AntonioMaggi <maggia1013@gmail.com>
2024-01-15 15:02:43 +0100

    Added Toast

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet, RouterModule } from '@angular/router';
+import { ToastrService } from 'ngx-toastr'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterModule],
+  imports: [CommonModule, RouterOutlet, RouterModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
+  constructor(private toastr: ToastrService) { }
+
   title = 'recipe';
+
+  ngOnInit() {
+    this.toastr.success('Hello world!', 'Toastr fun!');
+    function ngOnInit() {
+      throw new Error('Function not implemented.');
+    }
+}
 }
