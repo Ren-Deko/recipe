29a9630216dcdf2a1c957151291ac429e465f029
AntonioMaggi <maggia1013@gmail.com>
2024-01-15 10:28:22 +0100

    created dummy data in body

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,22 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  // Add ElementRef to your constructor
+  constructor(private el: ElementRef) {}
+
+  // Listen for scroll events on the window
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: Event) {
+    // Get the current scrollable section and the next section
+    const currentSection = this.el.nativeElement.querySelector('.scroll-section');
+    const nextSection = this.el.nativeElement.querySelector('.recipes-section');
+
+    // Check if the user has scrolled to the bottom of the current section
+    if (currentSection.scrollHeight - currentSection.scrollTop === currentSection.clientHeight) {
+      // If the bottom is reached, show the next section
+      nextSection.style.display = 'block';
+    }
+  }
 }
