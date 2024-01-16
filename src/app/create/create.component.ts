7ee4ee600509e996d0e8dfb3e9597ea6c912aa81
AntonioMaggi <maggia1013@gmail.com>
2024-01-16 15:24:31 +0100

    Added form on 'Create New'

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,12 +1,63 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, FormArray, Validators, ReactiveFormsModule } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.css'
 })
 export class CreateComponent {
+  
+  recipeForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private toastr: ToastrService) {
+    this.recipeForm = this.fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      ingredients: this.fb.array([]),
+      directions: this.fb.array([])
+    });
+
+    // Add the initial input fields
+    this.addIngredient();
+    this.addDirection();
+  }
+
+  get ingredients() {
+    return this.recipeForm.get('ingredients') as FormArray;
+  }
+
+  get directions() {
+    return this.recipeForm.get('directions') as FormArray;
+  }
+
+  addIngredient() {
+    this.ingredients.push(this.fb.control('', Validators.required));
+  }
+
+  removeIngredient(index: number) {
+    this.ingredients.removeAt(index);
+  }
+
+  addDirection() {
+    this.directions.push(this.fb.control('', Validators.required));
+  }
+
+  removeDirection(index: number) {
+    this.directions.removeAt(index);
+  }
+
+  // Implement reordering methods here
+  // ...
+
+  onSubmit() {
+    // Handle form submission
+    console.log(this.recipeForm.value);
+    // Show toast notification
+    this.toastr.success('Recipe Added');
+  }
 }
