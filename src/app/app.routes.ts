46eac2bd32c9796cb22299aaaf52fb2ead3356eb
AntonioMaggi <maggia1013@gmail.com>
2024-01-19 14:56:00 +0100

    Added child routes in profile component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,16 +4,30 @@ import { AboutComponent } from './about/about.component';
 import { ProfileComponent } from './profile/profile.component';
 import { HomeComponent } from './home/home.component';
 import { RecipeFormComponent } from './recipe-form/recipe-form.component';
+import { PersonalComponent } from './personal/personal.component';
+import { NewsletterComponent } from './newsletter/newsletter.component';
+import { SavedComponent } from './saved/saved.component';
+import { MyrecipesComponent } from './myrecipes/myrecipes.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
-    { path: 'profile', component: ProfileComponent },
     { path: 'recipe', component: RecipeFormComponent },
+    {path: 'profile', component: ProfileComponent,
+    children: [
+        { path: 'personal', component: PersonalComponent },
+        { path: 'newsletter', component: NewsletterComponent },
+        { path: 'saved', component: SavedComponent },
+        { path: 'myrecipes', component: MyrecipesComponent },
+        { path: 'reviews', component: ReviewsComponent }
+    ]},
 ];
 
+
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot(routes), RouterModule.forChild(routes)],
     exports: [RouterModule]
   })
   export class AppRoutingModule { }
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,19 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterOutlet, Router, NavigationStart, NavigationEnd, RouterModule, Routes } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule, RouterOutlet, RouterModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
+
 export class ProfileComponent {
   username = 'user123';
   userEmail = 'user@example.com';
+
+  loading = false;
 
 saveUsername() {
 throw new Error('Method not implemented.');
 }
 
+constructor(private router: Router) {
+  this.router.events.pipe(
+    filter(event => event instanceof NavigationStart || event instanceof NavigationEnd)
+  ).subscribe(event => {
+    if (event instanceof NavigationStart) {
+      this.loading = true;
+    } else if (event instanceof NavigationEnd) {
+      this.loading = false;
+    }
+  });
+
+}
 }
